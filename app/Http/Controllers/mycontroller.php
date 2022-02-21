c30c7beb4756bda8f035a001433697a05fed93e0
Asim-123 <MuhammadAsim2222@yahoo.com>
2022-02-21 05:29:47 -0800

    first commit

---
+++ app/Http/Controllers/mycontroller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class mycontroller extends Controller
+{
+
+function indexcontroller(){
+
+return view('index');
+
+}
+
+
+
+}
